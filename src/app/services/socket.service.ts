0f521853148de308d31da710479d7ec9c399e527
Anto <georges.indrianjafy@gmail.com>
2018-03-06 17:43:33 +0200

    add socket io

---
+++ src/app/services/socket.service.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import * as io from 'socket.io-client';
+import {AuthService} from './auth.service';
+import {UserModel} from '../models/user.model';
+import {FileService} from './file.service';
+
+@Injectable()
+export class SocketService {
+
+  socket;
+  user: UserModel;
+
+  constructor (private authService: AuthService, private fileService: FileService) {
+    console.log('SocketService');
+    authService.user.subscribe((user) => {
+      if (user) {
+        this.updateConnection(user);
+      }
+    });
+
+    console.log('socket');
+    this.socket = io('http://localhost:81/');
+
+    this.socket.once('connect', () => {
+      console.log('connected');
+    });
+
+    this.socket.on('hello', (data) => {
+      console.log('ws', data);
+    });
+
+    this.socket.on('file added', (data) => {
+      this.fileService.fileAdded(data);
+    });
+
+    this.socket.on('file changed', (data) => {
+      this.fileService.fileChanged(data);
+    });
+
+    this.socket.on('files deleted', data => {
+      this.fileService.filesDeleted(data);
+    });
+  }
+
+  updateConnection (user) {
+    if (this.user) {
+      this.socket.emit('change user', {userId: user.uid, oldUserId: this.user.uid});
+    } else {
+      this.socket.emit('new user', {userId: user.uid});
+    }
+    this.user = user;
+  }
+}
